b029c51ab6631f5202141a7c055501377e9f0d81
cesartidrick <cesartidrick@gmail.com>
2022-04-05 13:26:22 -0600

    commit tree a6a6e33cbcff950eb3e881bf98f75619e1095c61

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+let num1 = 31
+
+let num2 = 2
+
+let multiply = num1 * num2
+
+multiply;
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 25
+
+const num4 = 4
+
+let mod = num4 % num3;
+
+let max = Math.max(5, 9 , 13, 20);
+
